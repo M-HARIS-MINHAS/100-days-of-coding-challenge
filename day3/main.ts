ba1801740c927477579207a213fb73b1e6ee0df9
M-HARIS-MINHAS <minhasharis81@gmail.com>
2024-05-26 23:31:08 +0500

    Add files via upload

--- day3/main.ts
+++ day3/main.ts
@@ -1,1 +1,32 @@
 
+//question no 7 :Number Eight: Write addition, subtraction, multiplication, and division operations that each 
+//result in the number 8. Be sure to enclose your operations in print statements to see the results.
+console.log(6+2);//addition
+console.log(10-2);//subtraction
+console.log(4*2);//multiplication
+console.log(16/2);//division
+
+
+
+
+
+//question no 8 :You should create four lines that look like this:
+
+//console.log(5 + 3)
+
+//Your output should simply be four lines with the number 8 appearing once on each line
+
+
+console.log(5 + 3);
+console.log(4 * 2);
+console.log(10 - 2);
+console.log(16 / 2);
+
+
+
+
+
+//question no 9:Favorite Number: Store your favorite number in a variable. Then, using that variable, 
+//create a message that reveals your favorite number. Print that message.
+let favouriteNumber:number =25;
+console.log(`my favourite number is ${favouriteNumber}`)
