c6dd593148979326056ee42b87ccbaba820bc116
M-HARIS-MINHAS <minhasharis81@gmail.com>
2024-05-26 23:12:10 +0500

    Add files via upload

---
+++ day1/main.js
@@ -0,0 +1,11 @@
+"use strict";
+// day one of coding
+Object.defineProperty(exports, "__esModule", { value: true });
+//question no 2:code
+let personName = "Eric";
+console.log(`Hello ${personName},would you like to learn some python today?`);
+//question no 3:code
+let userName = "m haris";
+console.log(userName.toUpperCase());
+console.log(userName.toLocaleLowerCase());
+console.log(userName.charAt(0).toUpperCase() + userName.slice(1).toLowerCase());
--- day1/main.ts
+++ day1/main.ts
@@ -1,1 +1,17 @@
+// day one of coding
 
+
+//question 2 : Personal Message: Store a person’s name in a variable,
+// and print a message to that person. Your message should be simple, 
+// such as, “Hello Eric, would you like to learn some Python today?”
+ let personName: string = "Eric";
+ console.log(`Hello ${personName},would you like to learn some python today?`);
+ 
+ 
+ 
+ //question 3 : Name Cases: Store a person’s name in a variable,
+//and then print that person’s name in lowercase,uppercase, and titlecase.
+let userName: string = "m haris";
+console.log(userName.toUpperCase());
+console.log(userName.toLocaleLowerCase());
+console.log(userName.charAt(0).toUpperCase()+userName.slice(1).toLowerCase());
